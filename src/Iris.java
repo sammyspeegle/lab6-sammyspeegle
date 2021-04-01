a0855bf6969e9031a97904fe8ff97383ad602272
sammyspeegle <speesr20@wfu.edu>
2021-04-01 10:12:02 -0400

    50%

--- src/Iris.java
+++ src/Iris.java
@@ -1,2 +1,17 @@
 public class Iris {
+
+    public String toString(){
+
+        //returns a string version of the information stored in the class
+    }
+    //@Override
+
+    public int compareTo(Iris Iris2){
+
+    }
+
+    public boolean isLessThan(Iris Iris2){
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,45 @@ import java.util.ArrayList;
 public class Main {
 
     //Method declarations
+
+
+    public static void switchMethod(ArrayList<Iris> a, int f, int s){
+        int temp = a.get(f);
+        a.set(f, a.get(s));
+        a.set(s, temp);
+    }
+
+
     public static void BubbleSort(ArrayList<Iris> a, int size){
-        //fix me
+
+
+        boolean swapped;
+        for(int i = 0; i<a.size()-1; i++){
+            swapped = false;
+            for (int j = 0; j<a.size() - 1 - i; j++){
+                if(a.get(j)>a.get(j+1)){
+                    switchMethod(a, j, j+1);
+                }
+            }
+
+            //fix me
+        }
     }
     public static void mergeSort(ArrayList<Iris> a, ArrayList<Iris> tmp, int left, int right){
+     int middle = (right-left)/2;
+      int array1[] = new int [middle];
+      int array2[] = new int[a.size() - middle];
+
+      for (int i = 0; i<middle; i++){
+          array1[i] = a[i];
+      }
+
+      for(int i = middle; i< right; i++){
+          array2[i] = a[i];
+      }
+
+
+
         //fix me
     }
     public static void mergeSortedLists(ArrayList<Iris> a, ArrayList<Iris> tmp, int left, int middle, int right){
