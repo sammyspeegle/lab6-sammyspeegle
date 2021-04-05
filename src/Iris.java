5f6eaa8238ffa6cb78183395047e628542af898d
sammyspeegle <speesr20@wfu.edu>
2021-04-04 21:48:31 -0400

    no graphs

--- src/Iris.java
+++ src/Iris.java
@@ -1,40 +1,120 @@
+import java.util.Comparator;
+
+
 public class Iris {
 
     //declare variables
-    int sepalLength;
-    int sepalWidth;
-    int petalLength;
-    int petalWidth;
+    double sepalLength;
+    double sepalWidth;
+    double petalLength;
+    double petalWidth;
+    String species;
 
     //default constructor
     public Iris(){
-        sepalLength = 0;
-        sepalWidth = 0;
-        petalLength = 0;
-        petalWidth = 0;
+        sepalLength = 0.0;
+        sepalWidth = 0.0;
+        petalLength = 0.0;
+        petalWidth = 0.0;
+        species = "NA";
+
     }
 
     //parameterized constructor
-    public Iris(int sepalLength, int sepalWidth, int petalLength. int petalWidth){
-
-
+    public Iris(double sepalLength, double sepalWidth, double petalLength, double petalWidth, String species){
+        this.sepalLength = sepalLength;
+        this.sepalWidth = sepalWidth;
+        this.petalLength = petalLength;
+        this.petalWidth = petalWidth;
+        this.species = species;
     }
+
+    //get methods
+    public double getSepalLength(){
+        return sepalLength;
+    }
+
+    public double getSepalWidth(){
+        return sepalWidth;
+    }
+
+    public double getPetalLength(){
+        return petalLength;
+    }
+
+    public double getPetalWidth(){
+        return petalWidth;
+    }
+
+    public String getSpecies(){
+        return species;
+    }
+
+    //set methods
+    public void setSepalLength(double sepalLength){
+        this.sepalLength = sepalLength;
+    }
+
+    public void setSepalWidth(double sepalWidth){
+        this.sepalWidth = sepalWidth;
+    }
+
+    public void setPetalLength(double petalLength){
+        this.petalLength = petalLength;
+    }
+
+    public void setPetalWidth(double petalWidth){
+        this.petalWidth = petalWidth;
+    }
+
+    public void setSpecies(String species){
+        this.species = species;
+    }
+
 
     //toString method
+    @Override //WHAT DID SHE WANT HERE????
     public String toString(){
-        return "Sepal Length: " + sepalLength + " Sepal Width: " + sepalWidth + " Petal Length: " + petalLength + " Petal Width: " + petalWidth;
+        return "Sepal Length: " + getSepalLength() + " Sepal Width: " + getSepalWidth() + " Petal Length: " + getPetalLength() + " Petal Width: " + getPetalWidth() + " Species: " + getSpecies();
         //returns a string version of the information stored in the class
     }
-    //@Override
 
-    public int compareTo(Iris Iris2){
-        if(sepalLengthIris1< sepalLengthIris2){
-            Iris1< Iris2;
+    public int Comparator(Iris Iris2){
+        irisComparator irisCompare = new irisComparator();
+        return irisCompare.compare(this, Iris2);
+    }
+
+
+    class irisComparator implements Comparator<Iris>{
+
+        //compareTo method// only supposed to be one parameter???
+
+        public int compare(Iris Iris1, Iris Iris2){
+
+            //doube check dont need and
+            int compareSepal = Double.compare(Iris1.getSepalLength(), Iris2.getSepalLength());
+            int comparePetal = Double.compare(Iris1.getPetalLength(), Iris2.getPetalLength());
+
+            if(compareSepal == 0){
+                return comparePetal;
+            }
+            else{
+                return compareSepal;
+            }
+
+
         }
     }
 
-    public boolean isLessThan(Iris Iris2){
 
+   public boolean isLessThan(Iris Iris2){
+            irisComparator iCompare = new irisComparator();
+            if(iCompare.compare(this, Iris2)> 0){
+                return false;
+            }
+            else{
+                return true;
+            }
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
-import java.lang.reflect.Array;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
@@ -7,69 +9,153 @@ public class Main {
 
 
     public static void switchMethod(ArrayList<Iris> a, int f, int s){
-        int temp = a.get(f);
+        Iris temp = a.get(f);
         a.set(f, a.get(s));
         a.set(s, temp);
     }
 
 
     public static void BubbleSort(ArrayList<Iris> a, int size){
-
-
         boolean swapped;
-        for(int i = 0; i<a.size()-1; i++){
+        for(int i = 0; i<size-1; i++){
             swapped = false;
-            for (int j = 0; j<a.size() - 1 - i; j++){
-                if(a.get(j)>a.get(j+1)){
+            for (int j = 0; j<size - 1 - i; j++){
+                if(!a.get(j).isLessThan(a.get(j+1))){
                     switchMethod(a, j, j+1);
+                    swapped = true;
                 }
+            }
+            if(!swapped){
+                break;
             }
 
             //fix me
         }
     }
+
+
+    //change parameter to Iris
     public static void mergeSort(ArrayList<Iris> a, ArrayList<Iris> tmp, int left, int right){
-     int middle = (right-left)/2;
-      int array1[] = new int [middle];
-      int array2[] = new int[a.size() - middle];
 
-      for (int i = 0; i<middle; i++){
-          array1[i] = a[i];
-      }
+        if(left<right){
+            int middle = (left+right)/2;
 
-      for(int i = middle; i< right; i++){
-          array2[i] = a[i];
-      }
+            mergeSort(a, tmp, left, middle);
+            mergeSort(a, tmp, middle+1, right);
 
+            mergeSortedLists(a, tmp, left, middle, right);
 
+        }
 
         //fix me
     }
-    public static void mergeSortedLists(ArrayList<Iris> a, ArrayList<Iris> tmp, int left, int middle, int right){
-        //fix me
-    }
 
-    public static void main(String [] args){
-        //.....
+    public static void mergeSortedLists(ArrayList<Iris> a, ArrayList<Iris> tmp, int left, int middle, int right) {
+        int first = left;
+        int second = middle + 1;
 
+        tmp = new ArrayList<Iris>();
 
+        while(first<middle+1 && second<=right){
+            if(a.get(first).isLessThan(a.get(second))){
+                tmp.add(a.get(first));
+                first = first + 1;
+            }
+            else {
+                tmp.add(a.get(second));
+                second = second + 1;
+            }
 
-        //....
-        ArrayList<Iris> list=new ArrayList<Iris>();   // list to be sorted
+        }
+
+        while(first<middle+1){
+            tmp.add(a.get(first));
+            first = first + 1;
+        }
+
+        while(second<=right){
+            tmp.add(a.get(second));
+            second = second + 1;
+        }
+
+        int third = left;
+        for(int k=0; k<tmp.size(); k++){
+            a.set(third, tmp.get(k));
+            third = third + 1;
+        }
+
+        }
+
+    public static <myFile> void main(String [] args){
+        ArrayList<Iris> list=new ArrayList<Iris>(); // list to be sorted
         ArrayList<Iris> tmp=new ArrayList<Iris>();   // temporary workspace
+
+        for(int i =0; i<list.size();i++){
+            tmp.add(list.get(i));
+        }
+
+
+        //read from file
+        FileInputStream myFile = null;
+        Scanner reader = null;
+
+        //try and catch to see if the file is not found
+        try {
+            myFile = new FileInputStream("fish-iris.csv.txt");
+            reader = new Scanner(myFile);
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not open input file - ending program");
+            System.exit(1);
+        }
+
+        //printing label line
+        System.out.println(reader.nextLine());
+
         //fill list
-        //....
-        //...
+        for (int i =0; i<100; i++){
+            String[] nextLine = reader.nextLine().split(",");
+            double sepalLength = Double.parseDouble(nextLine[0]);
+            double sepalWidth = Double.parseDouble(nextLine[1]);
+            double petalLength = Double.parseDouble(nextLine[2]);
+            double petalWidth = Double.parseDouble(nextLine[3]);
+            String species = nextLine[4];
+            list.add(new Iris(sepalLength, sepalWidth, petalLength, petalWidth, species));
+        }
+
+        int k = 0;
+        while(list.size()<100){
+            list.add(list.get(k%list.size()));
+            k++;
+        }
+
+
+       //NEED TO CLOSE THE READER yes
+
 
         //Create a copy from list for Bubble sort
         ArrayList <Iris> list2=new ArrayList<Iris>();
         for(int i=0;i<list.size();i++)
             list2.add(list.get(i));
 
+
+
+
+        long startTime = System.nanoTime();
         // sort list using mergesort
-        mergeSort(list, tmp, 0, list.size());
+        mergeSort(list, tmp, 0, list.size()-1);
+        long endTime = System.nanoTime();
+        System.out.println("Time of MergeSort: " + (endTime-startTime)+ "ns");
+
+        for(Iris i : list){
+            System.out.println(i.toString());
+        }
+
+        startTime = System.nanoTime();
         //sort list2 using Bubble sort
         BubbleSort(list2, list2.size());
+        endTime = System.nanoTime();
+        System.out.println("Time of BubbleSort: " + (endTime-startTime)+ "ns");
+
 
            }
 }
